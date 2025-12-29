9c3581617791255cb0fe33ff7c6ed13282016037
Jesus Ordosgoitty <jesuodz@gmail.com>
2025-12-29 01:33:00 -0400

    fix asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     // distDir: "./dist", // Changes the build output directory to `./dist/`.
     basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
     images: { unoptimized: true },
-    assetPrefix: process.env.NODE_ENV === "production" ? "./" : undefined,
+    // assetPrefix: process.env.NODE_ENV === "production" ? "./" : undefined,
 };
 
 export default nextConfig;
